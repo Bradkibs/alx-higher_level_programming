8aeb268e9ee6678d9362342087eaadf6a110ef87
Bradley Kibs <Bradleykibwana@gmail.com>
2022-09-20 20:19:35 +0300

    hot fix

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -2,12 +2,19 @@
 import random
 from sys import exit
 number = random.randint(-10000, 10000)
-number = str(number)
-if (number[0] == '-'):
-    print(f"Last digit of {number} is -{number[-1]} and is less than 6 and not 0")
-elif (number[-1] > '5'):
-    print(f"Last digit of {int(number)} is {number[-1]} and is greater than 5")
-elif (number[-1] < '6' and number[-1] != '0'):
-    print(f"Last digit of {int(number)} is {number[-1]} and is less than 6 and not 0")
-elif (number[-1] == '0'):
-    print(f"Last digit of {int(number)} is {number[-1]} and is 0")
+if number > 0:
+    new_num = number % 10
+elif number < 0:
+    new_num = (number % 10) - 10
+else:
+    new_num = number
+while (new_num > 10):
+    new_num = new_num % 10
+if (number < 0):
+    print(f"Last digit of {number} is {new_num} and is less than 6 and not 0")
+elif (new_num > 5):
+    print(f"Last digit of {number} is {new_num} and is greater than 5")
+elif (new_num < 6 and new_num != 0):
+    print(f"Last digit of {number} is {new_num} and is less than 6 and not 0")
+elif (new_num == 0):
+    print(f"Last digit of {number} is {new_num} and is 0")
