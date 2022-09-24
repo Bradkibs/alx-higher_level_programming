17ea109a19fae406de805e40275fe78be13b3c61
Bradley Kibs <Bradleykibwana@gmail.com>
2022-09-24 09:43:31 +0300

    changed return none to pass

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -4,7 +4,7 @@
 
 def print_reversed_list_integer(my_list=[]):
     if len(my_list) == 0:
-        return None
+        pass
     rev_list = my_list[::-1]
     for item in rev_list:
         print("{:d}".format(item))
