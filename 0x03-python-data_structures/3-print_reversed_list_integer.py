843c87cce7de16262687d45bf6f64ec24cfd0862
Bradley Kibs <Bradleykibwana@gmail.com>
2022-09-24 09:46:38 +0300

    removed defined var

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -4,7 +4,6 @@
 
 def print_reversed_list_integer(my_list=[]):
     if len(my_list) == 0:
-        pass
-    rev_list = my_list[::-1]
-    for item in rev_list:
+        return None
+    for item in rev_list[::-1]:
         print("{:d}".format(item))
