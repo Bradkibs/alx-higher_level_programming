0164d0510444cf0d853bbc263abcda67ca7269af
Bradley Kibs <Bradleykibwana@gmail.com>
2022-09-24 09:51:51 +0300

    fixing pycodestyle error

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -2,6 +2,7 @@
 
 """replacing an element in a list at a specific position without modifying the original list"""
 
+
 def new_in_list(my_list, idx, element):
     if (idx < 0 or idx > (len(my_list) - 1)):
         return my_list
