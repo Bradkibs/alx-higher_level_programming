90e11927eb33ca7a5852740314374d7f8a05fcf9
Bradley Kibs <Bradleykibwana@gmail.com>
2022-09-27 10:52:04 +0300

    fixing pycodestyle

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -3,7 +3,7 @@
 # removes all characters of c and C from a string
 
 def no_c(my_string):
-    new =[]
+    new = []
     for item in range(len(my_string)):
         if my_string[item] != 'c' and my_string[item] != 'C':
             new.append(my_string[item])
