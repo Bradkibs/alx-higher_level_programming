d571108e61b3b18f42445b7b5435cf63079da5a7
Bradley Kibs <Bradleykibwana@gmail.com>
2022-09-27 10:50:44 +0300

    removing c

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -6,6 +6,6 @@ def no_c(my_string):
     new =[]
     for item in range(len(my_string)):
         if my_string[item] != 'c' and my_string[item] != 'C':
-            new += my_string[item]
-        new_str = [i.join('') for i in new]
-        print(new_str)
+            new.append(my_string[item])
+    new_str = "".join(new)
+    return new_str
