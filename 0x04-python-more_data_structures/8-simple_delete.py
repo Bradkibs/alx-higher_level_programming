d42059a8d9769a4812f8a867c9276fbf779c4fa2
Bradley Kibs <Bradleykibwana@gmail.com>
2022-09-28 18:38:33 +0300

    added if a key doesn't exist

--- 0x04-python-more_data_structures/8-simple_delete.py
+++ 0x04-python-more_data_structures/8-simple_delete.py
@@ -3,5 +3,7 @@
 #deletes a key value pair in a dictionary
 
 def simple_delete(a_dictionary, key=""):
+    if key not in a_dictionary.keys():
+        return (a_dictionary)
     a_dictionary.pop(key)
     return (a_dictionary)
