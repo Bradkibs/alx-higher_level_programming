46445e13429386c63acf6670dfa047405810b13e
Bradley Kibs <Bradleykibwana@gmail.com>
2022-10-11 18:23:31 +0300

    second task

---
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+def safe_print_integer(value):
+    """ Print integers
+    Args:
+        value: what to print
+    Returns:
+        return true or false
+    """
+    try:
+        print("{:d}".format(value))
+    except Exception as error:
+        return False
+    else:
+        return True
