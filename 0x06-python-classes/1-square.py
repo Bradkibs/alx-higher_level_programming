86d90e0ccb95aef703d926d61d572e89eaaffcee
Bradley Kibs <Bradleykibwana@gmail.com>
2022-10-12 19:30:44 +0300

    creating private instance attributes

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+"""A generic square class"""
+
+
+class Square:
+    """A square class containing private instance attributes"""
+
+    def __init__(self, size):
+        self.__size = size
