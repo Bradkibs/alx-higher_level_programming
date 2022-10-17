6e0ec37a64b33a68a8e1e6ac1865d1b608e2eb6a
Bradley Kibs <Bradleykibwana@gmail.com>
2022-10-17 17:50:41 +0300

    Adding docstrings

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -4,15 +4,18 @@ class Rectangle:
     """ Defines the properties of a rectangle"""
 
     def __init__(self, width=0, height=0):
+        """ Initializing the class """
         self.__width = width
         self.__height = height
 
     @property
     def width(self):
+    """A getter for the width property"""
         return self.__width
 
     @width.setter
     def width(self, value):
+    """A setter for the width property"""
         if type(self.__width) is not int:
             raise TypeError("width must be an integer")
         if self.__width < 0:
@@ -21,10 +24,12 @@ class Rectangle:
 
     @property
     def height(self):
+        """A getter for the height property"""
         return self.__height
 
     @height.setter
     def height(self, value):
+        """A setter for the height property"""
         if type(self.__height) is not int:
             raise TypeError("height must be an integer")
         if self.__height < 0:
