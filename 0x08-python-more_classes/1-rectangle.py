96b540e54066c363f11052dab3986fd3ae3d4673
Bradley Kibs <Bradleykibwana@gmail.com>
2022-10-17 18:14:52 +0300

    fixing some errors in setter methods

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -7,8 +7,8 @@ class Rectangle:
     """ Defines the properties of a rectangle"""
 
     def __init__(self, width=0, height=0):
-        self.__width = width
-        self.__height = height
+        self.width = width
+        self.height = height
 
     @property
     def width(self):
@@ -18,9 +18,9 @@ class Rectangle:
     @width.setter
     def width(self, value):
         """A setter for the width property"""
-        if type(self.__width) is not int:
+        if type(value) is not int:
             raise TypeError("width must be an integer")
-        if self.__width < 0:
+        if value < 0:
             raise ValueError("width must be >= 0")
         self.__width = value
 
@@ -32,8 +32,8 @@ class Rectangle:
     @height.setter
     def height(self, value):
         """A setter for the height property"""
-        if type(self.__height) is not int:
+        if type(value) is not int:
             raise TypeError("height must be an integer")
-        if self.__height < 0:
+        if value < 0:
             raise ValueError("height must be >= 0")
         self.__height = value
