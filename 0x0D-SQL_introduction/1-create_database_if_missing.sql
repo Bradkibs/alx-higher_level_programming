34d1ac7df7dcc083b63c8243bb7b4c5030ae85f6
Bradley Kibs <Bradleykibs@gmail.com>
2022-11-30 21:10:52 +0300

    creating a database only if missing

---
+++ 0x0D-SQL_introduction/1-create_database_if_missing.sql
@@ -0,0 +1,7 @@
+-- creates the database hbtn_0c_0 if it doesn't exist
+-- if it exists ignore
+CREATE DATABASE IF NOT EXISTS hbtn_0c_0;-- creates the database hbtn_0c_0 if it doesn't exist
+-- if it exists ignore
+CREATE DATABASE IF NOT EXISTS hbtn_0c_0;-- creates the database hbtn_0c_0 if it doesn't exist
+-- if it exists ignore
+CREATE DATABASE IF NOT EXISTS hbtn_0c_0;
