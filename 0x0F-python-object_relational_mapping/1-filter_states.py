5f6519825fbf9c7aa7431a4591317d54e6561023
Bradleykibs <Bradleykibs@gmail.com>
2022-12-24 19:16:45 +0300

    odering by id

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -12,7 +12,8 @@ if __name__ == "__main__":
     db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
                          passwd=argv[2], db=argv[3])
     cur = db.cursor()
-    cur.execute("SELECT id, name FROM states WHERE name LIKE 'N%' ORDER BY id")
+    cur.execute("""SELECT id, name FROM states WHERE name LIKE
+                BINARY 'N%' ORDER BY id""")
 
     for row in cur.fetchall():
         print(row)
