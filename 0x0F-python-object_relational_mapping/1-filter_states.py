4b3b8a3b7d22b5b9859b9fce1a6d57e51cc84668
Bradleykibs <Bradleykibs@gmail.com>
2022-12-24 19:09:30 +0300

    Using the LIKE to find specific patterns in a query

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+A script that lists all states with a name starting with N from the database
+    user, password and database are passed on as command line arguments
+    example: ./1-filter_states.py root root hbtn_0e_0_usa
+"""
+
+
+if __name__ == "__main__":
+    import MySQLdb
+    from sys import argv
+    db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
+                         passwd=argv[2], db=argv[3])
+    cur = db.cursor()
+    cur.execute("SELECT id, name FROM states WHERE name LIKE 'N%'")
+
+    for row in cur.fetchall():
+        print(row)
+    db.close()
