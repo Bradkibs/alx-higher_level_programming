1ecdc0f7d937ca93d01a2ffa9ebe3d5be79943a3
Bradleykibs <Bradleykibs@gmail.com>
2022-12-24 19:51:38 +0300

    adding BINARY keyword to make the query case sensitive

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -12,7 +12,7 @@ if __name__ == "__main__":
     db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
                          passwd=argv[2], db=argv[3])
     cur = db.cursor()
-    cur.execute("""SELECT * FROM states WHERE name='{}' ORDER
+    cur.execute("""SELECT * FROM states WHERE BINARY name='{}' ORDER
                    BY id""".format(argv[4]))
 
     for row in cur.fetchall():
