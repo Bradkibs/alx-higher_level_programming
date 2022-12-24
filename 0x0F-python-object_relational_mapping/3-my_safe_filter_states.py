116eccd44576e4f9a597eb19ec396204cdc9b072
Bradleykibs <Bradleykibs@gmail.com>
2022-12-24 20:10:30 +0300

    fixing pycodestyle error

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -13,7 +13,7 @@ if __name__ == "__main__":
                          passwd=argv[2], db=argv[3])
     cur = db.cursor()
     cur.execute("""SELECT * FROM states WHERE BINARY name = %s ORDER
-                   BY id""",(argv[4],))
+                   BY id""", (argv[4],))
 
     for row in cur.fetchall():
         print(row)
