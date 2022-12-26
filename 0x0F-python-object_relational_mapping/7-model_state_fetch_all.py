81f99e6315bc6bbd0ab06ef20c35409623024037
Bradleykibs <Bradleykibs@gmail.com>
2022-12-26 18:00:54 +0300

    using sqlalchemy to make queries

---
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+a python script that lists all state objects from the database
+using sqlalchemy
+"""
+
+
+from model_state import State, Base
+from sys import argv
+from sqlalchemy import create_engine
+from sqlalchemy.engine.url import URL
+from sqlalchemy.orm import sessionmaker
+
+
+if __name__ == "__main__":
+    url_params = { 'drivername' : 'mysql+mysqldb',
+        'username' : argv[1],
+        'password' : argv[2],
+        'host' : 'localhost',
+        'port' : 3306,
+        'database' : argv[3]}
+    db_engine = create_engine(URL.create(**url_params))
+    Base.metadata.create_all(db_engine)
+    Session = sessionmaker(bind=db_engine)()
+    for row in Session.query(State).order_by(State.id):
+        print(row.id, row.name, sep=": ")
