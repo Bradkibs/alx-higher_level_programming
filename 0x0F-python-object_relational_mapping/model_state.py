7aac9d7e6f5a28a38e9ce983fe799d62d5e07b34
Bradleykibs <Bradleykibs@gmail.com>
2022-12-26 17:10:52 +0300

    using sqlalchemy

---
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+using SQLAlchemy orm to make queries to mysqldb
+"""
+
+
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String, MetaData
+
+Meta = MetaData()
+Base = declarative_base(metadata=Meta)
+
+class State(Base):
+    """
+    A class state to define a mapped class
+    """
+    __tablename__ = 'states'
+    
+    id = Column(Integer, primary_key=True, unique=True, nullable=False)
+    name = Column(String(128), nullable = False)
