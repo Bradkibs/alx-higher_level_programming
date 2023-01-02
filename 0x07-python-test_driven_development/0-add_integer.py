70cf8673c0dd391081d8f1b19701f7f1cbb82ea5
Bradleykibs <Bradleykibs@gmail.com>
2023-01-02 13:09:41 +0300

    fixing pycodestyle error

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -3,15 +3,18 @@
 """ A function to add two integers """
 
 
-
 def add_integer(a, b=98):
-    """Adds two numbers namely a and b if given.
+    """
+    Adds two numbers namely a and b if given.
     If b is not given a default value of 98 is assigned to it
-    Returns the addition of two numbers as an integer"""
+    Returns the addition of two numbers as an integer
+    """
+
     if not isinstance(a, (int, float)):
         raise TypeError("a must be an integer")
     if not isinstance(b, (int, float)):
         raise TypeError("b must be an integer")
     a = int(a)
     b = int(b)
+
     return a + b
