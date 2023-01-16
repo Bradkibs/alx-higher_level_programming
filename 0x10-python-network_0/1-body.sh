15b95f5edefe78ea11de2929d00bc045dd602ef6
Bradleykibs <Bradleykibs@gmail.com>
2023-01-17 01:37:28 +0300

    rewriting it to a one liner

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 # sends a GET request and displays the body of respnce only if status is 200
-if [ "$(curl -sI -X GET "$1" | grep "HTTP" | cut -d ' ' -f2)" = '200' ];
-then curl -s "$1"; fi
+if [ "$(curl -sI -X GET "$1" | grep "HTTP" | cut -d ' ' -f2)" = '200' ];then curl -s "$1"; fi
