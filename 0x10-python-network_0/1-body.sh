4a53b3d554fddeec18cc91a2781631ffc021d367
Bradleykibs <Bradleykibs@gmail.com>
2023-01-17 01:35:23 +0300

    added request type

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # sends a GET request and displays the body of respnce only if status is 200
-if [ "$(curl -sI "$1" | grep "HTTP" | cut -d ' ' -f2)" = '200' ];
+if [ "$(curl -sI -X GET "$1" | grep "HTTP" | cut -d ' ' -f2)" = '200' ];
 then curl -s "$1"; fi
