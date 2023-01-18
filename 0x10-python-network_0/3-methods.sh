f7b147ccc2353a853569dc7f33d612cc13c6db85
Bradleykibs <Bradleykibs@gmail.com>
2023-01-18 19:55:02 +0300

    listing values on the Allow header response

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # displays all HTTP methods the server will accept
-curl -sI "$1" -X OPTIONS | grep "access-control-allow-methods" | cut -d' ' -f2
+sudo curl -sI "$1" -X OPTIONS | grep "access-control-allow-methods" | cut -d' ' -f2
