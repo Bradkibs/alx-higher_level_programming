1954ba991c0b35966a8bb066e9468ce49dd0e780
Bradleykibs <Bradleykibs@gmail.com>
2023-02-09 23:57:12 +0300

    same order of list

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -1,6 +1,9 @@
 #!/usr/bin/node
+// Prints all characters of a Star Wars movie
+
 const request = require('request');
 const url = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
+
 request(url, function (error, response, body) {
   if (!error) {
     const characters = JSON.parse(body).characters;
