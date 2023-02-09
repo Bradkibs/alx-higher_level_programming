dde21c1073cf28f9adc80e98768263a0750c83f2
Bradleykibs <Bradleykibs@gmail.com>
2023-02-09 17:27:00 +0300

    fixing semistandard errors

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -3,13 +3,12 @@
 const fs = require('fs').promises;
 const process = require('process');
 const filePath = process.argv[2];
-async function read(file) {
-	try {
-		const data = await fs.readFile(file, 'utf-8');
-		console.log(data.toString());
-	}
-	catch (error) {
-	console.log(error);
-}
+async function read (file) {
+  try {
+    const data = await fs.readFile(file, 'utf-8');
+    console.log(data.toString());
+  } catch (error) {
+    console.log(error);
+  }
 }
 read(filePath);
