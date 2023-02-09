2c3c1b27fab1ac0d816ab91ec250821ea1b5bcd9
Bradleykibs <Bradleykibs@gmail.com>
2023-02-09 22:40:39 +0300

    semi standard error

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -1,18 +1,17 @@
 #!/usr/bin/node
 const request = require('request');
 request(process.argv[2], (error, resp, body) => {
-	if (!error) {
-		const todos = JSON.parse(body);
-		const completed_task = {};
-		todos.forEach((todo) => {
-			if (todo.completed && completed_task[todo.userId] === undefined) {
-				completed_task[todo.userId] = 1;
-			} else if (todo.completed) {
-				completed_task[todo.userId] += 1;
-			}
-		});
-		console.log(completed_task);
-	}
+  if (!error) {
+    const todos = JSON.parse(body);
+    const completedTask = {};
+    todos.forEach((todo) => {
+      if (todo.completed && completedTask[todo.userId] === undefined) {
+        completedTask[todo.userId] = 1;
+      } else if (todo.completed) {
+        completedTask[todo.userId] += 1;
+      }
+    });
+    console.log(completedTask);
+  }
 }
 );
-
