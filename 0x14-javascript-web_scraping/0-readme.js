e0bf566fbdcb481ce89913a30ab2341591dea78a
Bradleykibs <Bradleykibs@gmail.com>
2023-02-09 17:53:23 +0300

    trying out callback version of fs

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,7 +1,16 @@
 #!/usr/bin/node
+const fs = require('fs');
+const file = process.argv[2];
+fs.readFile(file, 'utf-8', function (err, data) {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(data);
+  }
+});
 
+/*
 const fs = require('fs').promises;
-const process = require('process');
 const filePath = process.argv[2];
 async function read (file) {
   try {
@@ -12,3 +21,4 @@ async function read (file) {
   }
 }
 read(filePath);
+*/
