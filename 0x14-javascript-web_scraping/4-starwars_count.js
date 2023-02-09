e18a11f12634cb6e3d701311d37399416598b7e9
Bradleykibs <Bradleykibs@gmail.com>
2023-02-09 22:19:46 +0300

    ends with 18 character

---
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,18 @@
+#!/usr/bin/node
+
+const request = require('request');
+const url_char_id = 'https://swapi-api.alx-tools.com/api/people/18/';
+const url = process.argv[2];
+
+request(url, (error, response, body) => {
+  if (error) {
+    console.log(error);
+  } else {
+    const results = JSON.parse(body).results;
+    console.log(results.reduce((count, movie) => {
+      return movie.characters.find((character) => character.endsWith('/18/'))
+        ? count + 1
+        : count;
+    }, 0));
+  }
+});
