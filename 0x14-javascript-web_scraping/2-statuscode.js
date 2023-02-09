8c37fac2eff755ce1a771b04b7bac0a8b79d09b1
Bradleykibs <Bradleykibs@gmail.com>
2023-02-09 21:16:53 +0300

    status code

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -4,10 +4,10 @@ const request = require('request');
 const url = process.argv[2];
 
 request(url, (error, resp) => {
-	if (error) {
-		console.log(error);
-	} else {
-  console.log('code: ' + resp.statusCode);
-	}
+  if (error) {
+    console.log(error);
+  } else {
+    console.log('code: ' + resp.statusCode);
+  }
 }
 );
